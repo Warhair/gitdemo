7be522137a4956acbfc1e0f7ceb49b08cdf0669a
Warhair <tegzesmarton@gmail.com>
2023-09-20 12:51:50 +0200

    static metodus()

--- src/gitdemo/Gitdemo.java
+++ src/gitdemo/Gitdemo.java
@@ -6,9 +6,17 @@ public class Gitdemo {
 
     
     public static void main(String[] args) {
+        metodus();
         for (int i = 0; i < 10; i++) {
             
         }
+        if (true) {
+            
+        }
+    }
+
+    private static void metodus() {
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
